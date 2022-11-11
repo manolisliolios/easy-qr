28fcf6587b331cf7c52d96712a77d7b6f84524f7
Manolis Liolios <emmanouil.liolios@gmail.com>
2022-11-11 18:04:13 +0200

    typo fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@ import QRGenerator from "../components/QRGenerator.vue";
       <div class="w-[700px] max-w-full mx-auto text-center mt-12">
 
         <div class="intro">
-          <h2 class="text-xl font-bold mb-3">Your easiest way to create your QR Code</h2>
+          <h2 class="text-xl font-bold mb-3">The easiest way to create your QR Code</h2>
           <p class="text-sm">This tool was created to easily generate a QR code and download it in a format you can use to print, not a small and blurred png file!</p>
 
         </div>
